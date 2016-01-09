babc48fafb46902aac82147c65953de9991f01cf
Ahmad Shahwaiz <shahwaiz90@yahoo.com>
2016-01-09 23:52:16 +0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
-$con = mysql_connect("localhost","username","pass");
+$USERNAME = "";
+$PASSWORD = "";
+$DBNAME = "";
+$con = mysql_connect("localhost",$USERNAME,$PASSWORD );
 if(!$con)
 {
 	die("Connection Error");
 	
 }
-mysql_select_db("db-name",$con);  
+mysql_select_db($DBNAME,$con);  
 ?>
